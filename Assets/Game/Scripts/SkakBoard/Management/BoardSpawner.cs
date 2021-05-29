b9df6eefc8d0a62eb6fac2391d4af213111424dd
Yegor Zhumikov <yegor@yogurtthehor.se>
2021-05-29 14:40:28 +0300

    feat: move board containers creation to code

--- Assets/Game/Scripts/SkakBoard/Management/BoardSpawner.cs
+++ Assets/Game/Scripts/SkakBoard/Management/BoardSpawner.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Security.Cryptography;
 using Game.Scripts.SkakBoard.Entities;
 using Game.Scripts.SkakBoard.Squares;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Game.Scripts.SkakBoard.Management
 {
@@ -10,8 +12,8 @@ namespace Game.Scripts.SkakBoard.Management
     {
         private Board _board;
 
-        public Transform entitiesParent;
-        public Transform squaresParent;
+        private Transform _entitiesParent;
+        private Transform _squaresParent;
         
         public Square squarePrefab;
         public Piece.Piece piecePrefab;
@@ -19,22 +21,36 @@ namespace Game.Scripts.SkakBoard.Management
         public void Awake()
         {
             _board = GetComponent<Board>();
+            
+            _entitiesParent = CreateChildContainer("Entities");
+            _squaresParent = CreateChildContainer("Squares");
         }
+
+        private Transform CreateChildContainer(string name)
+        {
+            var go = new GameObject();
+            go.transform.parent = transform;
+            go.name = name;
+
+            return go.transform;
+        }
+        
 
         public Square SpawnSquare(int x, int y, SquareState squareState)
         {
-            var square = Instantiate(squarePrefab, _board.GetPositionFor(x, y), Quaternion.identity, squaresParent);
+            var square = Instantiate(squarePrefab, _board.GetPositionFor(x, y), Quaternion.identity, _squaresParent);
             square.state = squareState;
             
             square.SyncSprite();
             square.SetSorting(y);
+            square.name = $"Square ({x}, {y})";
 
             return square;
         }
 
         public Piece.Piece SpawnPiece(Vector2Int pos)
         {
-            var piece = Instantiate(piecePrefab, _board.GetPositionFor(pos), Quaternion.identity, entitiesParent);
+            var piece = Instantiate(piecePrefab, _board.GetPositionFor(pos), Quaternion.identity, _entitiesParent);
 
             if (_board.entityManager.Place(piece.GetComponent<BoardEntity>(), pos))
             {
